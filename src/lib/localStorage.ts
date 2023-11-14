a1fbf185df47d235bfa32a8e300752c20b6bffcb
andrew-fiel <andrew.w.fiel@gmail.com>
2023-11-13 23:45:12 -0700

    Revise session storage
    Webpack was freaking out

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -8,14 +8,21 @@ export type StoredGameState = {
 export const saveGameStateToSessionStorage = (
   gameState: StoredGameState
 ) => {
-  sessionStorage.setItem(gameStateKey, JSON.stringify(gameState))
+  if (sessionStorage) {
+    sessionStorage.setItem(gameStateKey, JSON.stringify(gameState))
+  }
 }
 
 export const loadGameStateFromSessionStorage = () => {
-  const state = sessionStorage.getItem(gameStateKey)
+  let state;
+  if (sessionStorage) {
+  state = sessionStorage.getItem(gameStateKey)
+  }
   return state ? (JSON.parse(state) as StoredGameState) : null
 }
 
 export const clearGameStateFromSessionStorage = () => {
+  if (sessionStorage) {
     sessionStorage.removeItem(gameStateKey)
+  }
 }
