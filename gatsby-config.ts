e4279587d18d3048d43b0ccad4863d9033f2ede5
andrew-fiel <andrew.w.fiel@gmail.com>
2023-11-15 22:25:05 -0700

    For real fix favicon
    Yep

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Christmas Wordle`,
+    description: `A wordly christmas wordle. Worlde.`,
+    author: `@andrew-fiel`,
+    siteUrl: `https://christmas.andrewfiel.me/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -28,15 +28,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `christmas-wordle`,
+        short_name: `wordle`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#FFFFFF`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/penguin-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
