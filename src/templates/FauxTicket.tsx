3f1ed5bdb82750e0c65fcd6fdecbd50919c94087
andrew-fiel <andrew.w.fiel@gmail.com>
2023-11-26 18:45:19 -0700

    Actually fix css things
    Confirmed with local production build this time

--- src/templates/FauxTicket.tsx
+++ src/templates/FauxTicket.tsx
@@ -1,15 +1,27 @@
 import React from "react"
 
+import '../css/wordle/Game.css'
 
 import Div100vh from 'react-div-100vh'
+
+// One-off ticket for displaying a free drink ticket
+// Most of the css is from: https://codingartistweb.com/2022/02/css-cup-animation-css-animation-tutorial/
 
 const Ticket = ({ pageContext }) => {
   return (
     <Div100vh>
-      <body className="lame-text">
-        {pageContext.text}
-      </body>
-      <div className="cup"/>
+      <div className='row'>
+        <div className='column'>
+          <body className="lame-text">
+            {pageContext.text}
+          </body>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='column'>
+          <div className="cup"/>
+        </div>
+      </div>
     </Div100vh>
   )
 }
